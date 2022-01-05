ed755487c4b61b8975f344fb2ef05c854aa75080
ldonita <ldonita@student.21-school.ru>
2022-01-05 03:20:32 +0300

    checklist checks ok

--- days/day01/ex00/main.cpp
+++ days/day01/ex00/main.cpp
@@ -6,12 +6,9 @@ int main()
     Zombie *heap = new Zombie("HEAP");
     randomChump("Chump");
     
-    Zombie *zombieHoard = new Zombie[42]; // Default constructor Zombie() is required
 
     // Deallocate with delete
     delete heap; // Calls destructor of the object
-    delete [] zombieHoard;
     // delete &stack; // Pointer being freed was not allocated
 
-    // NOTE: Zaz: 'References are like a non-void constant'
 }
--- days/day01/ex00/randomChump.cpp
+++ days/day01/ex00/randomChump.cpp
@@ -4,15 +4,8 @@
 
 void    randomChump(std::string name)
 {
-    // HEAD 
-    // Zombie *zombie;
-
-    // zombie = newZombie(name);
-    // if (!zombie)
-    //     return ;
-    // zombie->announce();
-    // delete zombie; // No leaks
 
     Zombie zombie = Zombie(name);
+    zombie.announce();
     // destroyer
 }
