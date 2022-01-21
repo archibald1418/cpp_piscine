65e5be77ed57223e012bfb4a51aa73dfab5123a6
ldonita <ldonita@student.21-school.ru>
2022-01-21 10:11:58 +0300

    main.cpp ok

--- days/day04/ex03/main.cpp
+++ days/day04/ex03/main.cpp
@@ -6,15 +6,21 @@
 #include "Cure.hpp"
 #include "MateriaSource.hpp"
 
+#include <unistd.h>
+
 int main()
 {
     IMateriaSource* src = new MateriaSource();
     src->learnMateria(new Ice());
     src->learnMateria(new Cure());
+    src->learnMateria(new Cure());
+    src->learnMateria(new Cure());
+    src->learnMateria(new Ice());
     ICharacter* me = new Character("me");
     AMateria* tmp;
     tmp = src->createMateria("ice");
     me->equip(tmp);
+    tmp = src->createMateria("cure");
     tmp = src->createMateria("cure");
     me->equip(tmp);
     ICharacter* bob = new Character("bob");
