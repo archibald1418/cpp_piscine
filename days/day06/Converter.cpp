aeea9968fe026dd7e3bd5602e1605f15d828b20c
archibald1418 <liam141810@gmail.com>
2022-01-27 18:33:09 +0300

    started testing ok

---
+++ days/day06/Converter.cpp
@@ -0,0 +1,138 @@
+#include "Converter.hpp"
+
+Converter::Converter() : _num(0)
+{
+    
+}
+Converter::~Converter()
+{
+    
+}
+Converter::Converter (const Converter& other)
+{
+    *this = other;
+}
+Converter& Converter::operator=(const Converter& other)
+{
+    if (this == &other)
+        return (*this);
+    this->_num = other._num;
+    return (*this);
+}
+
+void Converter::setNum(double _num)
+{
+    this->_num = _num;
+}
+
+double Converter::getNum()const
+{
+    return (this->_num);
+}
+
+void    Converter::convertPrintChar()const 
+{
+    std::cout << "char: ";
+    if (!isascii(static_cast<int>(this->_num)))
+        throw Impossible();
+    if (!isprint(static_cast<int>(this->_num)))
+        throw NotDisplayable();
+    
+    std::cout << static_cast<char>(this->_num) << std::endl;
+}
+
+void    Converter::convertPrintInt()const
+{
+    std::cout << "int: ";
+
+    if (this->_num > std::numeric_limits<int>::max() || \
+        this->_num < std::numeric_limits<int>::min() || \
+        std::isnan(this->_num) || std::isinf(this->_num))
+        throw Impossible();
+    
+    std::cout << static_cast<int>(this->_num) << std::endl;
+}
+
+void    Converter::convertPrintFloat()const
+{
+    bool minus = this->_num < -1e-8;
+    bool plus = this->_num > 1e-8;
+
+    std::cout << "float: ";
+    if (((std::fabs(this->_num - std::numeric_limits<float>::max()) < 1e-8) || \
+        (std::fabs(this->_num - std::numeric_limits<float>::min() < 1e-8))) && \
+        ((!std::isnan(static_cast<float>(this->_num))) && \
+        !std::isinf(static_cast<float>(this->_num))))
+        throw Impossible();
+    else if (std::isnan(static_cast<float>(this->_num)))
+        std::cout << "nanf" << std::endl;
+    else if (std::isinf(static_cast<float>(this->_num)))
+    {
+        if (minus)
+            std::cout << "-";
+        else if (plus)
+            std::cout << "+";
+        std::cout << "inf";
+    }
+    else
+        std::cout << static_cast<float>(this->_num);
+    std::cout << "f" << std::endl;
+}
+
+void    Converter::convertPrintDouble()const
+{
+    bool minus = this->_num < -1e-14;
+    bool plus = this->_num > 1e-14;
+
+    std::cout << "double: ";
+    if (std::isnan(static_cast<double>(this->_num)))
+        std::cout << "nan";
+    if (std::isinf(static_cast<double>(this->_num)))
+    {
+        if (minus)
+            std::cout << "-";
+        else if (plus)
+            std::cout << "+";
+        std::cout << "inf";
+    }
+    else if ( 
+        ((std::fabs(this->_num - std::numeric_limits<double>::max()) < 1e-14) || \
+        (std::fabs(this->_num - std::numeric_limits<double>::min() < 1e-14))))
+            throw Impossible();
+    else
+        std::cout << static_cast<double>(this->_num);
+    
+    std::cout << std::endl;
+}
+
+void    Converter::convert(const std::string& name)const
+{
+    std::string params[4] = {"char", "int", "float", "double"};
+    Converter::f func[4] = {
+        &Converter::convertPrintChar,
+        &Converter::convertPrintInt,
+        &Converter::convertPrintFloat,
+        &Converter::convertPrintDouble
+    };
+
+    int i = 0;
+    while (i < 4)
+    {
+        if (name == params[i])
+        {
+            try 
+            {
+                (this->*(func[i]))();
+            }
+            catch (std::exception &e)
+            {
+                std::cout << e.what() << std::endl;
+            }
+            break;
+        }
+        i++;
+    }
+    if (i == 4)
+        std::cout << "unknown type" << std::endl;
+}
+
---
+++ days/day06/Converter.hpp
@@ -0,0 +1,45 @@
+#ifndef CONVERTER_H
+# define CONVERTER_H
+
+# include <iostream>
+#include <exception>
+#include <cmath>
+#include <limits>
+#include <iomanip>
+
+class Converter
+{
+private:
+    double _num;
+public:
+    Converter();
+    ~Converter();
+    Converter (const Converter& other);
+    Converter& operator=(const Converter& other);
+
+    void setNum(double num);
+    double getNum()const;
+
+    void convertPrintChar()const;
+    void convertPrintInt()const;
+    void convertPrintFloat()const;
+    void convertPrintDouble()const;
+
+    void convert(const std::string& name)const;
+
+    typedef void (Converter::*f)()const;
+    
+};
+
+class NotDisplayable : public std::exception
+{
+        public: virtual const char *what() const throw(){return "Not displayable";}
+};
+
+class Impossible : public std::exception
+{
+    public: virtual const char *what() const throw(){return "Impossible to convert";}
+};
+
+
+#endif
