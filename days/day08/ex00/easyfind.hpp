4b5379ea889bd924bac012b1de78b9a4f51919ab
ldonita <ldonita@student.21-school.ru>
2022-01-30 14:10:20 +0300

    iter works ok

--- days/day08/ex00/easyfind.hpp
+++ days/day08/ex00/easyfind.hpp
@@ -1,8 +1,62 @@
 #ifndef EASYFIND_H
 # define EASYFIND_H
+#include <algorithm>
+#include <list> 
+#include <vector> 
+#include <deque> 
+#include <iterator>
+#include <string>
+#include <exception>
+
+// template < class T, class Alloc = allocator<T> > class deque;
+// template<
+//     typename Container, 
+//         template<
+//             class = int, // NOTE: this works
+//             class Alloc = std::allocator<int> >
+//     class T >
 
 
+// template<
+//     typename Container, 
+//         template<
+//             typename,
+//             class Alloc = std::allocator<int> >
+//     class T >
 
-#include "easyfind.tpp"
+class NotFound : public std::exception
+{
+    public:
+        std::string msg;
+        virtual const char* what() const throw()
+        {
+            return (this->msg.c_str());
+        };
+    
+    NotFound(std::string msg) : msg(msg){};
+    virtual ~NotFound() throw(){return;};
+    
+};
 
+
+template < typename T >
+typename T::iterator easyfind(T& container, int n)
+{
+    typename T::iterator it;
+
+    it = std::find(container.begin(), container.end(), n);
+    if (it == container.end())
+        throw NotFound("Number " + std::to_string(n) + " not found");
+    return (it);
+}
+
+template < typename T >
+const typename T::iterator easyfind(const T& container, int n)
+{
+    const typename T::iterator it;
+        it = std::find(container.begin(), container.end(), n);
+        if (it == container.end())
+            throw NotFound("Number " + std::to_string(n) + " not found");
+    return (it);
+}
 #endif
--- days/day08/ex00/main.cpp
+++ days/day08/ex00/main.cpp
@@ -2,9 +2,37 @@
 #include <list>
 #include <deque>
 #include <vector>
+#include <iostream>
 #include "easyfind.hpp"
 
 int main()
 {
     
+    std::srand(time(NULL));
+
+    std::vector<int> vector;
+
+    int size = std::rand() % 15;
+    int num = std::rand() % 15;
+
+    for (int i = 0; i < size; i++)
+        vector.push_back(std::rand() % 15);
+    
+    for (int i = 0; i < size; i++)
+        std::cout << "v< " << vector.at(i) << " >" << std::endl;
+    
+    std::vector<int>::iterator iter;
+
+    try
+    {
+        iter = easyfind(vector, num);
+        std::cout << "Found it!: ";
+        std::cout << *iter << std::endl;
+        std::cout << "at index #" << std::distance(vector.begin(), iter) << std::endl;
+    }
+    catch (NotFound& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+
 }
